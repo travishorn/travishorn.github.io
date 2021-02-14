8755b2fc68a46b0d2c7b7e1a806e4ebce4f799e7
Travis Horn <travis@travishorn.com>
2021-02-14 14:39:04 -0600

    Main content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,23 @@
 <template>
-  <div class="bg-gradient-to-tr from-stellar-red to-stellar-blue py-20 px-2">
+  <div class="min-h-screen bg-gradient-to-tr from-stellar-red to-stellar-blue py-20 px-2">
     <site-header />
-    <Nuxt />
+
+    <div class="container mx-auto px-5 py-20 bg-white rounded-b-lg">
+      <Nuxt />
+    </div>
+
+    <site-footer />
   </div>
 </template>
 
 <script>
 import SiteHeader from "@/components/SiteHeader";
+import SiteFooter from "@/components/SiteFooter";
 
 export default {
   components: {
-    SiteHeader
+    SiteHeader,
+    SiteFooter
   }
 }
 </script>
