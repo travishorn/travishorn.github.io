0ec01feea2337526eeda6e3c5024ec08a247df70
travishorn <travishorn@users.noreply.github.com>
2021-02-15 00:18:14 +0000

    deploy: dcc9d028f417a2470098946cbcd3841725981e83

---
+++ _nuxt/7040b68.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{197:function(n,e,t){"use strict";t.r(e);var l=t(17),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  resume\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
