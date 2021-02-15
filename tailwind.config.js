8f5a9e8cec99daf4bf50017aa3529ed5d9ff095b
Travis Horn <travis@travishorn.com>
2021-02-15 15:15:08 -0600

    Darken shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
       },
       fontFamily: {
         sans: ["Quicksand", ...defaultTheme.fontFamily.sans]
-      }
+      },
+      boxShadow: {
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.2), 0 2px 4px -1px rgba(0, 0, 0, 0.12)',
+        lg: "0 10px 15px -3px rgba(0, 0, 0, 0.33), 0 4px 6px -2px rgba(0, 0, 0, 0.167)"
+      },
     }
   }
 };
