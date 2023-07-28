7951c15acbeaae08ec723c18d7e4492c378dd1b3
Travis Horn <travis@travishorn.com>
2023-07-28 17:25:50 -0500

    Switch to SvelteKit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Travis Horn</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl font-bold underline">Travis Horn</h1>
