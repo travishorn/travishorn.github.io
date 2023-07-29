b33beb47fa29af66e443e98fd3ff6ab0f5056873
Travis Horn <travis@travishorn.com>
2023-07-29 13:24:54 -0500

    Add doodle

---
+++ src/lib/icons/Data.svelte
@@ -0,0 +1,44 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 120 120"
+	stroke-width="3"
+	stroke-miterlimit="10"
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	{...$$props}
+>
+	<rect x="2.86" y="2.65" width="47" height="36.75" rx="5.96" ry="5.96" />
+	<rect x="68.01" y="81.05" width="47" height="36.75" rx="5.96" ry="5.96" />
+
+	<circle stroke-width="0" fill="currentColor" cx="75.37" cy="88.79" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="91.07" cy="88.79" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="106.77" cy="88.79" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="75.37" cy="99.43" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="91.07" cy="99.43" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="106.77" cy="99.43" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="75.37" cy="110.06" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="91.07" cy="110.06" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="106.77" cy="110.06" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="83.22" cy="94.11" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="98.92" cy="94.11" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="83.22" cy="104.01" r="2.29" />
+	<circle stroke-width="0" fill="currentColor" cx="98.92" cy="104.01" r="2.29" />
+
+	<rect
+		x="67.67"
+		y="10.38"
+		width="21.28"
+		height="21.28"
+		rx="1.66"
+		ry="1.66"
+		transform="translate(37.8 -49.22) rotate(45)"
+	/>
+
+	<path
+		d="m53.89,99.43H7.44c-2.73,0-4.58-1.25-4.58-5.32v-25.49c0-4.58,2.94-8.83,10.03-8.94,7.09-.11,90.29,0,96.17,0s7.74-2.51,7.74-8.29v-25.62c0-4.58-1.64-4.8-4.03-4.74s-14.72-.05-19.41,0"
+	/>
+
+	<line x1="38.95" y1="83.45" x2="53.67" y2="98.93" />
+	<line x1="38.95" y1="115.07" x2="53.67" y2="100.03" />
+</svg>
--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,16 +1,21 @@
 <script>
 	import { EnvelopeIcon } from 'heroicons-svelte/24/outline';
+	import DataIcon from '$lib/icons/Data.svelte';
 </script>
 
 <section
 	class="flex flex-col lg:flex-row container px-3 mx-auto justify-center items-center lg:items-start"
 >
-	<h1 class="text-4xl lg:text-8xl font-bold text-center lg:text-right lg:pt-28">
+	<h1 class="text-4xl lg:text-8xl font-black text-center lg:text-right lg:pt-28">
 		Travis<br class="hidden lg:block" />
 		Horn
 	</h1>
 
-	<div class="flex flex-col gap-4 lg:gap-8 pt-10 lg:pt-60 lg:order-last lg:max-w-sm">
+	<div class="flex flex-col gap-4 lg:gap-8 pt-10 lg:pt-32 lg:order-last lg:max-w-sm">
+		<div class="hidden lg:flex justify-center pb-10">
+			<DataIcon class="w-24 h-24 text-rosa-gray-400 -rotate-45" />
+		</div>
+
 		<div class="font-bold text-2xl">
 			Based in St. Louis, I'm a data scientist and software engineer.
 		</div>
